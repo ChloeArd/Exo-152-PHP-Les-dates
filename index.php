439f6e9ef05962e0f3a994f8591c58c19d2d40e5
ChloeArd <chloe.ardoise@gmail.com>
2021-01-27 13:11:46 +0100

    Exercice 152

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 
 // Premiere ligne
 
+$day = date('w');
+echo $day." ième jour de la semaine <br>";
 
+$dateTime = new DateTime('2018-12-10');
+echo $dateTime->format('Y.m.d')."<br>";
 
-
-
-
+$hourTime = new DateTime('11:35:07');
+echo $hourTime->format('H:i:s');
